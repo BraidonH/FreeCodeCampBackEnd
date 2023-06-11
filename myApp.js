5e3c8d9095ed51d2c2d420470e497ec59e07b912
BraidonH <braidonhjames@icloud.com>
2023-06-11 16:58:59 -0500

    setup

--- myApp.js
+++ myApp.js
@@ -1,15 +1,23 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 
+app.use('/public', express.static(__dirname + '/public'));
 
 
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({"message": "hello json".toUpperCase()})
+    } else {
+        res.json({"message": "Hello json"})
 
+    }
+})
 
-
-
-
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
 
 
 
